f96914960a6dd1bc8c203435bded713706b7ba90
barsbold-coding <barsbold.coding@gmail.com>
2026-03-20 15:58:47 +0800

    Wire session service into gRPC server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,25 +1,46 @@
 package main
 
 import (
-  "log"
-  "net"
+	"context"
+	"log"
+	"net"
+	"os"
 
-  "google.golang.org/grpc"
-  pb "github.com/barsboldb/ascend-backend/gen/workout"
-  "github.com/barsboldb/ascend-backend/internal/server"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	pb "github.com/barsboldb/ascend-backend/gen/session"
+	"github.com/barsboldb/ascend-backend/internal/db"
+	"github.com/barsboldb/ascend-backend/internal/server"
 )
 
 func main() {
-  lis, err := net.Listen("tcp", ":8888")
-  if err != nil {
-    log.Fatalf("failed to listen: %/", err)
-  }
 
-  grpcServer := grpc.NewServer()
-  pb.RegisterWorkoutServiceServer(grpcServer, &server.WorkoutServer{})
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
 
-  log.Println("Server running on :8888")
-  if err := grpcServer.Serve(lis); err != nil {
-    log.Fatalf("failed to serve: %v", err)
-  }
+	pool, err := pgxpool.New(context.Background(), dbURL)
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer pool.Close()
+
+	queries := db.New(pool)
+
+	lis, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSessionServiceServer(grpcServer, server.NewSessionServer(queries))
+	reflection.Register(grpcServer)
+
+	log.Println("Server running on :8888")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
